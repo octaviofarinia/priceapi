d6fda20034d39e7c9801f4eeea94a675ec81c308
Octavio Fariña <octaviofarinia@gmail.com>
2023-10-25 14:25:13 +0200

    Initial Commit

---
+++ src/test/java/com/bcnc/priceapi/PriceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcnc.priceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
